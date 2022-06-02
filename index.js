ca01d48db6d359c60881536eb7f05e91fc23849f
honorbrute <106188784+honorbrute@users.noreply.github.com>
2022-06-02 18:43:10 -0400

    DRY
    Reduced two similar for loops into one that creates the listener events for the Dice selectors.

--- index.js
+++ index.js
@@ -29,52 +29,41 @@ let hitRolls = 0;
 let woundRolls = 0;
 let selectorValue = 0;
 
+createSelectors("wSelected", wSelectors)
+createSelectors("hSelected", hSelectors)
 
-// Event listeners for "Hit" selectors
-for (let i = 0; i < selectorSize; i++) {
-    hSelectors[i].addEventListener("click", function() {
-        let currentHSel = document.getElementsByClassName("hSelected");
-        let currentRolls = document.querySelectorAll(".rollSelected");
-        // console.log(currentRolls.length)
-        // Remove selected red functions
-        if (currentHSel.length > 0) {
-            currentHSel[0].classList.remove("hSelected")
-            for (let i =0; i < currentRolls.length; i++) {
-                currentRolls[i].classList.remove("rollSelected")
+/*
+Creates event listeners for the Dice Selectors.
+Highlights the dice that meets selector threshold.
+Updates X hits or X wounds.
+*/
+function createSelectors(selectorClass, selectorslisteners) {
+    for (let i = 0; i < selectorSize; i++) {
+        selectorslisteners[i].addEventListener("click", function() {
+            let currentWSel = document.getElementsByClassName(selectorClass);
+            let currentRolls = document.querySelectorAll(".rollSelected");
+            // console.log(currentRolls.length)
+            // Remove selected red functions
+            if (currentWSel.length > 0) {
+                currentWSel[0].classList.remove(selectorClass)
+                for (let i =0; i < currentRolls.length; i++) {
+                    currentRolls[i].classList.remove("rollSelected")
+                }
             }
-        }
-        this.classList.add("hSelected");
-        let selectorString = this.innerHTML.split("+");
-        selectorValue = parseInt(selectorString[0], 10)
-        selectDice(diceArray, selectorValue);
-        hitRolls = newDiceArray.length;
-        hDiceCountID.innerHTML = `= ${hitRolls} HITS`;
-    })
+            this.classList.add(selectorClass);
+            let selectorString = this.innerHTML.split("+");
+            selectorValue = parseInt(selectorString[0], 10)
+            selectDice(diceArray, selectorValue);
+            woundRolls = newDiceArray.length;
+            hitRolls = newDiceArray.length;
+            selectorClass === "wSelected" ? wDiceCountID.innerHTML = `= ${woundRolls} WOUNDS`
+                : selectorClass === "hSelected" ? hDiceCountID.innerHTML = `= ${hitRolls} HITS`
+                : null;
+            
+        })
+    }
 }
 
-// Event listeners for "Wound" selectors
-for (let i = 0; i < selectorSize; i++) {
-    wSelectors[i].addEventListener("click", function() {
-        console.log(`diceArray: ${diceArray}`)
-        let currentWSel = document.getElementsByClassName("wSelected");
-        let currentRolls = document.querySelectorAll(".rollSelected");
-        // console.log(currentRolls.length)
-        // Remove selected red functions
-        if (currentWSel.length > 0) {
-            currentWSel[0].classList.remove("wSelected")
-            for (let i =0; i < currentRolls.length; i++) {
-                currentRolls[i].classList.remove("rollSelected")
-            }
-        }
-        this.classList.add("wSelected");
-        let selectorString = this.innerHTML.split("+");
-        selectorValue = parseInt(selectorString[0], 10)
-        selectDice(diceArray, selectorValue);
-        woundRolls = newDiceArray.length;
-        wDiceCountID.innerHTML = `= ${woundRolls} WOUNDS`;
-        
-    })
-}
 
 // For auto hitters
 rollForWoundsID.addEventListener("click", rollWounds);
@@ -88,12 +77,12 @@ function rollWounds() {
 // Return array of indexes that meet selector criteria.
 function selectDice(diceRolls, diceSelector) {
     newDiceArray = diceRolls.reduce(function(indexes, currentRoll, index) {
-        console.log(`newDiceArray: ${newDiceArray}`);
         if (currentRoll >= diceSelector) {
             indexes.push(index)
         }
         return indexes;
     }, []);
+    console.log(`newDiceArray: ${newDiceArray}`);
 
     //Need to make this into a function highlightRolls()? .eq(i) in a forloop
     for (let i = 0; i < newDiceArray.length; i++) {
